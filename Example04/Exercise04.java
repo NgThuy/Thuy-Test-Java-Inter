b729fc86990b250d14b2af2a30d90df9831622e5
NgThuy <36691828+NgThuy@users.noreply.github.com>
2018-04-17 14:39:54 +0700

    Update Exercise04.java

--- Example04/Exercise04.java
+++ Example04/Exercise04.java
@@ -20,7 +20,7 @@ import java.util.Set;
  */
 public class Exercise04 {
 
-    public int rand(int min, int max) {
+    public int randNumber(int min, int max) {
         Random rd = new Random();
         int range = max - min + 1;
         int randomNum = min + rd.nextInt(range);
@@ -51,45 +51,49 @@ public class Exercise04 {
         // TODO code application logic here
         Exercise04 main = new Exercise04();
         Set<Point> setAllPoint = new HashSet<Point>();
-        Set<Point> setPointA = new HashSet<Point>();
-        Set<Point> setPointB = new HashSet<Point>();
-        Set<Point> setPointC = new HashSet<Point>();
-        //A(800,800)
+        Set<Point> setAllPointA = new HashSet<Point>();
+        Set<Point> setAllPointB = new HashSet<Point>();
+        Set<Point> setAllPointC = new HashSet<Point>();
+       
         do {
-            Point A = new Point(800, 800);
-            Point rdPointA = new Point(main.rand(0, 800), main.rand(0, 800));
-            double d1 = Math.sqrt(Math.pow(rdPointA.x - A.x, 2) + Math.pow(rdPointA.y - A.y, 2));
-            if (d1 < 400 && !setPointA.contains(rdPointA)) {
-                setPointA.add(rdPointA);
-                setAllPoint.add(rdPointA);
+         //A(800,800)
+            if (setAllPointA.size() < 8000) {
+                Point A = new Point(800, 800);
+                Point rdPointA = new Point(main.randNumber(0, 800), main.randNumber(0, 800));
+                double dA = Math.sqrt(Math.pow(rdPointA.x - A.x, 2) + Math.pow(rdPointA.y - A.y, 2));
+                if (dA < 400 && !setAllPointA.contains(rdPointA)) {
+                    setAllPointA.add(rdPointA);
+                    setAllPoint.add(rdPointA);
+
+                }
             }
-        } while (setPointA.size() < 8000);
-        //B(2400,800)
-        do {
-            Point B = new Point(4000, 800);
-            Point rdPointB = new Point(main.rand(2000, 4000), main.rand(0, 800));
-            double d2 = Math.sqrt(Math.pow(rdPointB.x - B.x, 2) + Math.pow(rdPointB.y - B.y, 2));
-            if (d2 < 500 && !setPointB.contains(rdPointB)) {
-                setPointA.add(rdPointB);
-                setAllPoint.add(rdPointB);
+         //B(4000,800)
+            if (setAllPointB.size() < 10000) {
+                Point B = new Point(4000, 800);
+                Point rdPointB = new Point(main.randNumber(2000, 4000), main.randNumber(0, 800));
+                double dB = Math.sqrt(Math.pow(rdPointB.x - B.x, 2) + Math.pow(rdPointV.y - B.y, 2));
+                if (dB < 500 && !setAllPointB.contains(rdPointB)) {
+                    setAllPointB.add(rdPointB);
+                    setAllPoint.add(rdPointB);
+                }
             }
-        } while (setPointA.size() < 10000);
         //C(2400,2400)
-        do {
-            Point C = new Point(2400, 2400);
-            Point rdPointC = new Point(main.rand(500, 2400), main.rand(0, 2400));
-            double d3 = Math.sqrt(Math.pow(rdPointC.x - C.x, 2) + Math.pow(rdPointC.y - C.y, 2));
-            if (d3 < 600 && !setPointC.contains(rdPointC)) {
-                setPointC.add(rdPointC);
-                setAllPoint.add(rdPointC);
+            if (setAllPointC.size() < 12000) {
+                Point C = new Point(2400, 2400);
+                Point rdPointC = new Point(main.randNumber(500, 2400), main.randNumber(0, 2400));
+                double dC = Math.sqrt(Math.pow(rdPointC.x - C.x, 2) + Math.pow(rdPointC.y - C.y, 2));
+                if (dC < 600 && !setAllPointC.contains(rdPointC)) {
+                    setAllPointC.add(rdPointC);
+                    setAllPoint.add(rdPointC);
+                }
             }
-        } while (setPointC.size() < 12000);
+        } while (setAllPointA.size() < 8000 || setAllPointB.size() < 10000 || setAllPointC.size() < 12000);
 
         main.writeFile("output4.txt", setAllPoint);
         System.out.println(setAllPoint.size());
-        System.out.println(setPointA.size());
-        System.out.println(setPointB.size());
-        System.out.println(setPointC.size());
+        System.out.println(setAllPointA.size());
+        System.out.println(setAllPointB.size());
+        System.out.println(setAllPointC.size());
     }
 
 }
